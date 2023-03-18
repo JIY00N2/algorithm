4e31f978019998567ee5dc15f24675f3360ec857
JiYoon <angella990825@gmail.com>
2023-03-18 16:06:50 +0900

    2023.03.18
    add 최빈값 구하기, 짝수는 싫어요, 각도기

--- Levle-0/day3.js
+++ Levle-0/day3.js
@@ -16,4 +16,91 @@ function solution(array) {
 }
 
 10. 최빈값 구하기
+내 풀이
+function solution(array) {
+    const sortedArray = array.sort((a,b)=>a-b);
+    let cnt = 0; //각 숫자의 갯수
+    let mode = -1; // 최빈값이 뭔지
+    let modeRepeatCnt = 0; //최빈값이 될 때 몇 번 반복해서 된건지
+    let repeatCnt = 0; //현재 똑같은 숫자가 몇번 반복했는지
+    let beforeNum = -1; //이전 숫자
+    let isDupMode = false; //최빈값이 중복 됐는지
+    while(cnt<array.length){
+        // 이전 숫자랑 지금 숫자랑 같지 않으면
+        if(beforeNum!==sortedArray[cnt]){
+            // 지금 숫자는 1번 반복
+            repeatCnt=1;
+        }else{
+            // 반복한 만큼 +1
+            repeatCnt+=1;
+        }
+        // 최빈값 중복 확인
+        // 지금 반복 횟수가 최빈값 반복 횟수랑 같을때
+        if(repeatCnt===modeRepeatCnt){
+            // 최빈값과 지금 숫자가 다를때
+            if(mode !==sortedArray[cnt]){
+                // 중복이 발생
+                 isDupMode = true;
+            }
+        }
+        // 지금 숫자의 반복횟수가 최빈값의 반복 횟수보다 크다면
+        if(repeatCnt>modeRepeatCnt){
+            // 최빈값은 지금 숫자
+            mode = sortedArray[cnt];
+            // 최빈값의 반복 횟수는 지금 숫자의 반복 횟수
+            modeRepeatCnt = repeatCnt;
+            isDupMode=false;
+        }
+        // 이전 숫자는 현재 숫자로 변경
+        beforeNum = sortedArray[cnt];
+        cnt+=1;
+        sortedArray[cnt];
+    }
+    if(isDupMode) return -1;
+    return mode;
+}
+//1. 앞에서부터 차례대로 원소를 확인하며 갯수를 센다
+//2. 최반값을 그때그때 기록한다.
+
+11. 짝수는 싫어요
+
+내 풀이
+function solution(n) {
+    const answer = [];
+    for(let i = 1; i <= n; i++){
+        if(i % 2 === 1){
+            answer.push(i);
+        }
+    }
+    return answer;
+}
+
+12. 각도기
+
+다른 사람 풀이
+#1 filter
+function solution(angle) {
+    return [0, 90, 91, 180].filter(x => angle>=x).length;
+}
+
+#2 삼항 연산자
+function solution(angle) {
+    return angle < 90 ? :1  angle === 90 ? 2 : angle < 180 ? 3 : 4;
+}
+
+내 풀이
+function solution(angle) {
+    if(angle > 0 && angle < 90){
+        answer = 1;
+    }else if(angle === 90){
+        answer = 2;
+    }
+    else if(angle === 180){
+        answer = 4;
+    }
+    else{
+        answer = 3;
+    }
+    return answer;
+}
  */
