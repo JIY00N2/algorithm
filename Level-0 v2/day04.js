67ec34509aa79e8d078431c2ac5f5ab1aa3b8cae
JiYoon <angella990825@gmail.com>
2023-05-07 16:26:04 +0900

    lv0 틀린것

---
+++ Level-0 v2/day04.js
@@ -0,0 +1,27 @@
+/*
+1.코드 처리하기
+function solution(code) {
+    let answer = '';
+    let mode = 0;
+    for(let i = 0; i<code.length; i++){
+        if(code[i] == "1"){
+            if(mode == 0)
+                mode = 1;
+            else
+                mode = 0;
+            continue;
+        }
+        if(mode == 1 && i % 2==1){
+            answer += code[i]
+        }
+        else if(mode == 0 && i % 2==0){
+            answer += code[i]
+        }
+    }
+
+    if(answer.length == 0){
+        return "EMPTY";
+    }
+    return answer;
+}
+ */
