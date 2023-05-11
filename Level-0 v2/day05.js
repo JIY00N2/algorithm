f13e000f72181e2a708fe799ec93f3ed8d96bad3
JiYoon <angella990825@gmail.com>
2023-05-11 10:07:21 +0900

    lv0 수열과 구간 쿼리 2

--- Level-0 v2/day05.js
+++ Level-0 v2/day05.js
@@ -7,4 +7,14 @@ function solution(arr, queries) {
     }
     return arr;
 }
+
+2. 수열과 구간 쿼리 2
+function solution(arr, queries) {
+    const answer = [];
+    for(const[s, e, k] of queries){
+        const tmp = arr.filter((v,i) => s <= i && i <= e && v > k).sort((a,b)=>a-b)[0];
+        answer.push(tmp ? tmp : -1);
+    }
+    return answer;
+}
  */
