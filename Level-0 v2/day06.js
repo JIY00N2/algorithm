175300c42eacb2e6fbd11b37abcc9b633836be9f
JiYoon <angella990825@gmail.com>
2023-05-12 22:20:00 +0900

    lv0 배열 만들기 2

--- Level-0 v2/day06.js
+++ Level-0 v2/day06.js
@@ -8,5 +8,31 @@ function solution(arr, queries) {
     return arr;
 }
 
+2. 배열 만들기 2
+function solution(l, r) {
+    var answer = [];
 
+    for(let i = l ; i <= r; i += 1) {
+        if(i.toString().split("").every(e => e == "5" || e == "0"))
+        answer.push(i);
+    }
+    return answer.length == 0 ? [-1] : answer;
+}
+
+function solution(l, r) {
+    let result = [];
+
+    for (let i = l; i <= r; i++) {
+        const numStr = i.toString();
+        if (numStr.match(/^[05]+$/)) {
+            result.push(i);
+        }
+    }
+
+    if (result.length === 0) {
+        result.push(-1);
+    }
+
+    return result;
+}
  */
