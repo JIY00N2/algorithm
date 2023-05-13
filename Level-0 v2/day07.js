6aa23f69a3ed002158104f4c8ce9e85898ad1c8f
JiYoon <angella990825@gmail.com>
2023-05-13 16:31:41 +0900

    lv0 주사위 게임3

---
+++ Level-0 v2/day07.js
@@ -0,0 +1,22 @@
+/*
+2. 주사위 게임 3
+function solution(a, b, c, d) {
+    if (a === b && a === c && a === d) return 1111 * a
+
+    if (a === b && a === c) return (10 * a + d) ** 2
+    if (a === b && a === d) return (10 * a + c) ** 2
+    if (a === c && a === d) return (10 * a + b) ** 2
+    if (b === c && b === d) return (10 * b + a) ** 2
+
+    if (a === b && c === d) return (a + c) * Math.abs(a - c)
+    if (a === c && b === d) return (a + b) * Math.abs(a - b)
+    if (a === d && b === c) return (a + b) * Math.abs(a - b)
+
+    if (a === b) return c * d
+    if (a === c) return b * d
+    if (a === d) return b * c
+    if (b === c) return a * d
+    if (b === d) return a * c
+    if (c === d) return a * b
+
+*/
