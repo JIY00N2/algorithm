1cf1bd73d1f8e56ef5f24ff6098b74a322b71945
JiYoon <angella990825@gmail.com>
2023-05-27 14:39:20 +0900

    lv0 두 수의 합, 배열의 원소 삭제하기

---
+++ Level-0 v2/day22.js
@@ -0,0 +1,15 @@
+/*
+2. 두 수의 합
+function solution(a, b) {
+    const bigA = BigInt(a);
+    const bigB = BigInt(b);
+
+    const result = (bigA + bigB).toString();
+    return result;
+    
+}
+4. 배열의 원소 삭제하기 
+function solution(arr, delete_list) {
+    return arr.filter(a => !delete_list.includes(a))
+}
+ */
