6e4c74a7f358695cf18251d696b200f8b5339e9d
JiYoon <angella990825@gmail.com>
2023-06-04 21:52:36 +0900

    코드 처리하기 다른 풀이

--- Level-0 v2/day04.js
+++ Level-0 v2/day04.js
@@ -24,4 +24,16 @@ function solution(code) {
     }
     return answer;
 }
+
+
+function solution(code) {
+    let odd = false
+    return Array.from(code).reduce((acc, v, i) => {
+        if (v === '1') {
+            odd = !odd
+            return acc
+        }
+        return (i % 2 === (odd ? 1 : 0)) ? acc + v : acc
+    }, '') || 'EMPTY'
+}
  */
