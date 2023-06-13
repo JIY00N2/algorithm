81a74166870d6bbb1fb76209ee53adb26fcee202
JiYoon <angella990825@gmail.com>
2023-06-13 18:56:03 +0900

    Heap: 최대힙 구현

--- algorithm/Heap/MaxHeap.js
+++ algorithm/Heap/MaxHeap.js
@@ -2,52 +2,60 @@ class MaxHeap {
   constructor() {
     this.heap = [null];
   }
-
+  size() {
+    return this.heap.length - 1;
+  }
+  swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
   push(value) {
     this.heap.push(value);
     let currentIndex = this.heap.length - 1;
     let parentIndex = Math.floor(currentIndex / 2);
     while (parentIndex !== 0 && this.heap[parentIndex] < value) {
-      const temp = this.heap[parentIndex];
-      this.heap[parentIndex] = value;
-      this.heap[currentIndex] = temp;
-
+      // 부등호 방향 변경
+      this.swap(parentIndex, currentIndex);
       currentIndex = parentIndex;
       parentIndex = Math.floor(currentIndex / 2);
     }
   }
-
   pop() {
-    const returnValue = this.heap[1];
-    this.heap[1] = this.heap.pop();
+    const max = this.heap[1]; // 변수명 변경
+    if (this.heap.length <= 2) {
+      this.heap = [null];
+    } else {
+      this.heap[1] = this.heap.pop();
+    }
 
     let currentIndex = 1;
-    let leftIndex = 2;
-    let rightIndex = 3;
+    let leftIndex = currentIndex * 2;
+    let rightIndex = currentIndex * 2 + 1;
+
+    if (!this.heap[leftIndex]) return max; // 부등호 방향 변경
+    else if (!this.heap[rightIndex]) {
+      if (this.heap[leftIndex] > this.heap[currentIndex]) {
+        // 부등호 방향 변경
+        this.swap(leftIndex, currentIndex);
+      }
+      return max; // 변수명 변경
+    }
 
     while (
-      this.heap[currentIndex] < this.heap[leftIndex] ||
-      this.heap[currentIndex] < this.heap[rightIndex]
+      this.heap[leftIndex] > this.heap[currentIndex] || // 부등호 방향 변경
+      this.heap[rightIndex] > this.heap[currentIndex] // 부등호 방향 변경
     ) {
-      if (this.heap[leftIndex] < this.heap[rightIndex]) {
-        const temp = this.heap[currentIndex];
-        this.heap[currentIndex] = this.heap[rightIndex];
-        this.heap[rightIndex] = temp;
-        currentIndex = rightIndex;
-      } else {
-        const temp = this.heap[currentIndex];
-        this.heap[currentIndex] = this.heap[leftIndex];
-        this.heap[leftIndex] = temp;
-        currentIndex = leftIndex;
-      }
+      const maxIndex =
+        this.heap[leftIndex] > this.heap[rightIndex] ? leftIndex : rightIndex; // 변수명 변경
+      this.swap(maxIndex, currentIndex);
+      currentIndex = maxIndex;
       leftIndex = currentIndex * 2;
       rightIndex = currentIndex * 2 + 1;
     }
-    return returnValue;
+    return max; // 변수명 변경
   }
 }
 
-const heap = new MaxHeap();
+const heap = new MaxHeap(); // 클래스명 변경
 heap.push(45);
 heap.push(36);
 heap.push(54);
