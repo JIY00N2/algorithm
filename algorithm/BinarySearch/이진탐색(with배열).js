f4a3ebe03a5804b144cec758a641b968c7095cd7
JiYoon <angella990825@gmail.com>
2023-06-14 18:08:08 +0900

    BS: 이진 탐색 배열로 구현

---
+++ algorithm/BinarySearch/이진탐색(with배열).js
@@ -0,0 +1,23 @@
+const array = [1, 1, 5, 124, 400, 599, 1004, 2876, 8712];
+
+function binarySearch(array, findValue) {
+  let left = 0;
+  let right = array.length - 1;
+  let mid = Math.floor((left + right) / 2);
+  while (left < right) {
+    if (array[mid] === findValue) {
+      return mid;
+    } else if (array[mid] < findValue) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+    mid = Math.floor((left + right) / 2);
+  }
+  return -1;
+}
+
+console.log(binarySearch(array, 2876)); // 7
+console.log(binarySearch(array, 1)); // 1
+console.log(binarySearch(array, 1004)); // 6
+console.log(binarySearch(array, 500)); // -1
