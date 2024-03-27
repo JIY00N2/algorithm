56e7e5a46be3f9994f6a7561e09ab382949b0e44
JIY00N2 <angella990825@gmail.com>
2024-03-27 13:22:06 +0900

    feat: List - TwoSum 시간복잡도 추가

--- all-in-one/List/TwoSum.js
+++ all-in-one/List/TwoSum.js
@@ -1,5 +1,6 @@
-// 반복문
+// 반복문 -> O(n^2)
 function twoSum(nums, target) {
+  // O(n^2)
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
@@ -12,11 +13,12 @@ function twoSum(nums, target) {
 
 console.log(twoSum([4, 1, 9, 7, 5, 3, 18], 14));
 
-// 투포인터
+// 투포인터 -> O(nlogn)
 function twoSumTwoPointer(nums, target) {
-  nums.sort((a, b) => a - b);
+  nums.sort((a, b) => a - b); // O(nlogn)
   let left = 0;
   let right = nums.length - 1;
+  // O(n)
   while (left < right) {
     if (nums[left] + nums[right] < target) {
       left++;
