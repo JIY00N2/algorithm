7f5816b73818ebe3868220b1b0017bdbd19b3955
JIY00N2 <angella990825@gmail.com>
2024-03-27 13:17:18 +0900

    feat: List - TwoSum (투포인터)

--- all-in-one/List/TwoSum.js
+++ all-in-one/List/TwoSum.js
@@ -11,3 +11,22 @@ function twoSum(nums, target) {
 }
 
 console.log(twoSum([4, 1, 9, 7, 5, 3, 18], 14));
+
+// 투포인터
+function twoSumTwoPointer(nums, target) {
+  nums.sort((a, b) => a - b);
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    if (nums[left] + nums[right] < target) {
+      left++;
+    } else if (nums[left] + nums[right] > target) {
+      right--;
+    } else {
+      return 'True';
+    }
+  }
+  return 'False';
+}
+
+console.log(twoSumTwoPointer([4, 1, 9, 7, 5, 3, 18], 14));
