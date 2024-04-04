0591e212e897ea14a0f0f359d403c021c3c522d4
JIY00N2 <angella990825@gmail.com>
2024-04-04 15:57:32 +0900

    feat: 리트코드/LowestCoomonAncestorOfABinaryTree/postorder

---
+++ v3/Tree/LowestCommonAncestorOfABinaryTree.js
@@ -0,0 +1,29 @@
+// postorder
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(root, p, q) {
+  if (root === null) {
+    return null;
+  }
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+  if (root.val === p.val || root.val === q.val) {
+    return root;
+  } else if (left && right) {
+    return root;
+  } else {
+    return left || right;
+  }
+}
---
+++ v3/Tree/Tree.md
@@ -0,0 +1,1 @@
+### 1. 리트코드 [Lowest Common Ancestor Of A Binary Tree](https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/) postorder
