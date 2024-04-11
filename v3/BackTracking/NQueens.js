c83869ccb54ead4825d82bb7026d4ec4734785dc
JIY00N2 <angella990825@gmail.com>
2024-04-11 18:31:37 +0900

    feat: 리트코드/NQueens/백트래킹

--- v3/BackTracking/NQueens.js
+++ v3/BackTracking/NQueens.js
@@ -1,6 +1,59 @@
 // DFS + 백트래킹
 // 퀸의 공격 범위는 가로, 세로, 대각선
 
+function solveNQueens2(n) {
+  const answer = [];
+
+  function drawGraph(curr) {
+    let arr = [];
+    let str = '';
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        if (curr[i] === j) {
+          str += 'Q';
+        } else {
+          str += '.';
+        }
+      }
+      arr.push(str);
+      str = '';
+    }
+    answer.push(arr);
+  }
+
+  function isValid(currentCol, curr) {
+    const currentRow = curr.length;
+    for (let i = 0; i < currentRow; i++) {
+      if (currentCol === curr[i]) {
+        return false;
+      }
+      if (Math.abs(currentCol - curr[i]) === Math.abs(currentRow - i)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function backtracking(currentRow, curr) {
+    if (currentRow === n) {
+      return drawGraph(curr);
+    }
+
+    for (let col = 0; col < n; col++) {
+      if (isValid(col, curr)) {
+        curr.push(col);
+        backtracking(currentRow + 1, curr);
+        curr.pop();
+      }
+    }
+  }
+
+  backtracking(0, []);
+  return answer;
+}
+
+console.log(solveNQueens2(4));
+
 function solveNQueens(n) {
   const answer = [];
 
@@ -44,7 +97,6 @@ function solveNQueens(n) {
     for (let i = 0; i < n; i++) {
       if (isPossible(i, cur)) {
         cur.push(i);
-        console.log(cur); // [1,3,0,2]
         backtrack(cur);
         cur.pop();
       }
