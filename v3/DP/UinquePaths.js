ea37421c760ff51bc0fa3829931bce01f1bcb3d3
JIY00N2 <angella990825@gmail.com>
2024-04-12 16:43:24 +0900

    feat: 리트코드/UniquePaths/DP(bottomup)

--- v3/DP/UinquePaths.js
+++ v3/DP/UinquePaths.js
@@ -1,3 +1,24 @@
+// 내 풀이
+function uniquePaths(m, n) {
+  function dp() {
+    const temp = Array.from({ length: m }, () => Array(n).fill(0));
+    for (let i = 0; i < m; i++) {
+      temp[i][0] = 1;
+    }
+    for (let i = 0; i < n; i++) {
+      temp[0][i] = 1;
+    }
+
+    for (let i = 1; i < m; i++) {
+      for (let j = 1; j < n; j++) {
+        temp[i][j] = temp[i - 1][j] + temp[i][j - 1];
+      }
+    }
+    return temp[m - 1][n - 1];
+  }
+  return dp();
+}
+
 // DP top down dfs
 function uniquePaths(m, n) {
   const memo = Array.from({ length: m }, () => Array(n).fill(-1));
