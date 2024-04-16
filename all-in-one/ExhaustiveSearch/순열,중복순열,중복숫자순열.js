70cad363a1234bf81a7ed07597bf6c0ec180a592
JIY00N2 <angella990825@gmail.com>
2024-04-16 18:00:24 +0900

    feat: 백트래킹 중복 숫자 순열 추가

--- all-in-one/ExhaustiveSearch/순열,중복순열,중복숫자순열.js
+++ all-in-one/ExhaustiveSearch/순열,중복순열,중복숫자순열.js
@@ -32,3 +32,49 @@ function permute(nums) {
 const nums = [1, 2, 3];
 console.log(permute(nums));
 // [[ 1, 2, 3 ], [ 1, 3, 2 ], [ 2, 1, 3 ], [ 2, 3, 1 ], [ 3, 1, 2 ], [ 3, 2, 1 ]]
+
+// 중복 숫자 순열
+function permuteUnique(nums) {
+  const result = [];
+  const k = nums.length;
+  const map = new Map(); // Map(2) { 1 => 2, 2 => 1 }
+
+  nums.forEach((num) => {
+    if (map.has(num)) {
+      map.set(num, map.get(num) + 1);
+    } else {
+      map.set(num, 1);
+    }
+  });
+
+  function backtracking(cur) {
+    if (cur.length === k) {
+      result.push([...cur]);
+      return;
+    }
+
+    for (let [key, value] of map.entries()) {
+      if (value > 0) {
+        cur.push(key);
+        map.set(key, value - 1);
+
+        backtracking(cur);
+
+        map.set(key, value);
+        cur.pop();
+      }
+    }
+  }
+
+  backtracking([]);
+
+  return result;
+}
+
+/** 
+Input: nums = [1,1,2]
+Output:
+[[1,1,2],
+ [1,2,1],
+ [2,1,1]]
+ */
