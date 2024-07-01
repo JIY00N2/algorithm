550d963ee75c347fdd4629d77b49fb52e4e4a0ac
JIY00N2 <angella990825@gmail.com>
2024-07-01 18:02:07 +0900

    feat: 다른 풀이 추가

--- v3/List/겹치는건싫어.js
+++ v3/List/겹치는건싫어.js
@@ -9,18 +9,35 @@ const input = require('fs')
 const [n, k] = input[0];
 const arr = input.slice(1)[0];
 
+// let left = 0;
+// let right = 0;
+// let answer = 0;
+// const map = new Map();
+
+// while (left <= right && right < n) {
+//   while (map.get(arr[right]) === k) {
+//     map.set(arr[left], map.get(arr[left]) - 1);
+//     left++;
+//   }
+//   answer = Math.max(answer, right - left + 1);
+//   map.set(arr[right], (map.get(arr[right]) ?? 0) + 1);
+//   right++;
+// }
+
+// console.log(answer);
+
 let left = 0;
 let right = 0;
 let answer = 0;
-const map = new Map();
+const obj = {};
 
 while (left <= right && right < n) {
-  while (map.get(arr[right]) === k) {
-    map.set(arr[left], map.get(arr[left]) - 1);
+  while (obj[arr[right]] === k) {
+    obj[arr[left]]--;
     left++;
   }
   answer = Math.max(answer, right - left + 1);
-  map.set(arr[right], (map.get(arr[right]) ?? 0) + 1);
+  obj[arr[right]] = (obj[arr[right]] ?? 0) + 1;
   right++;
 }
 
